a6d5be3bc2463c3f4a3e6ac2076fecea3ad74a91
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-30 19:48:31 -0300

    refactor: muda nome da classe de autenticação

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.betrybe.agrix.ebytr.staff.controlller;
+
+import com.betrybe.agrix.ebytr.staff.dto.AuthDto;
+import com.betrybe.agrix.ebytr.staff.dto.TokenDto;
+import com.betrybe.agrix.ebytr.staff.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Auth controller.
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  private final AuthenticationManager authenticationManager;
+  private final TokenService tokenService;
+
+  /**
+   * Instantiates a new Auth controller.
+   *
+   * @param authenticationManager the authentication manager
+   * @param tokenService          the token service
+   */
+  @Autowired
+  public AuthController(AuthenticationManager authenticationManager, TokenService tokenService) {
+    this.authenticationManager = authenticationManager;
+    this.tokenService = tokenService;
+  }
+
+  /**
+   * Login token dto.
+   *
+   * @param authDto the auth dto
+   * @return the token dto
+   */
+  @PostMapping("/login")
+  public TokenDto login(@RequestBody AuthDto authDto) {
+    UsernamePasswordAuthenticationToken userPassword =
+        new UsernamePasswordAuthenticationToken(
+            authDto.username(), authDto.password()
+        );
+    Authentication auth = authenticationManager.authenticate(userPassword);
+    String token = tokenService.generateToken(auth.getName());
+
+    return new TokenDto(token);
+  }
+}
