3937653e9e45ba80ca320c24195de0455d45a1ad
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-30 23:19:55 -0300

    feat: cria testes unitários da service de fertilizer

--- src/main/java/com/betrybe/agrix/ebytr/staff/model/repository/FertilizerRepository.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/model/repository/FertilizerRepository.java
@@ -2,9 +2,11 @@ package com.betrybe.agrix.ebytr.staff.model.repository;
 
 import com.betrybe.agrix.ebytr.staff.model.entity.Fertilizer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 /**
  * The interface Fertilizer repository.
  */
+@Repository
 public interface FertilizerRepository extends JpaRepository<Fertilizer, Long> { }
