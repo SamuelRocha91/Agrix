bb5079ddbd9f6d1ca9aaa34068833dd0d42dba42
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-01 09:54:15 -0300

    feat: adiciona validações em authDto

--- src/main/java/com/betrybe/agrix/ebytr/staff/dto/AuthDto.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/dto/AuthDto.java
@@ -1,7 +1,17 @@
 package com.betrybe.agrix.ebytr.staff.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * The type Auth dto.
  */
-public record AuthDto(String username, String password) {
+public record AuthDto(
+    @NotBlank(message = "Name is Required")
+    String username,
+
+    @Size(min = 8, message = "Password must have at least 8 characters")
+    @NotBlank(message = "Password is Required")
+    String password
+) {
 }
